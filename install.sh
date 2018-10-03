9923ad1f4eb62549569ef19660b94430ee6a5c84
Lukas Besch <connect@lukasbesch.com>
2018-10-03 15:26:52 +0200

    Improvements

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 sudo easy_install pip && \
 sudo pip install ansible && \
 ansible-galaxy install -r requirements.yml && \
-echo "Enter MAS details in config.yml"
-echo "ansible-playbook main.yml --skip-tags=\"post-provision\”"
-echo "sudo ansible-playbook main.yml --tags=\"post-provision\”"
+ansible-playbook main.yml -i inventory -K -v --skip-tags="mas"
